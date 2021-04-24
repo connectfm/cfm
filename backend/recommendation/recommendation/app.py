d0a4e073a267ad9ea69401b9c3d8988f277b5fa6
Ryan Tatton <rdt17@protonmail.com>
2021-04-24 11:27:00 -0400

    Adds proper response and error handling
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- backend/recommendation/recommendation/app.py
+++ backend/recommendation/recommendation/app.py
@@ -1,6 +1,7 @@
 import jsonpickle
 import logging
 import os
+from typing import Any, Dict
 
 import model
 import recommend
@@ -17,13 +18,28 @@ logger = logging.getLogger(__name__)
 
 def handle(event, context):
 	logger.debug(f'## ENV VARS\n{jsonpickle.encode(dict(**os.environ))}')
-	logger.debug(f'## EVENT\n{jsonpickle.encode(event)}')
+	logger.debug(f'## EVENT\n{(event := jsonpickle.encode(event))}')
 	logger.debug(f'## CONTEXT\n{jsonpickle.encode(context)}')
-	recommendation = _handle(event['body'])
-	return {'body': recommendation, 'statusCode': 200}
+	try:
+		recommendation = _handle(event['body'])
+		response = _response(200, recommendation, event)
+	except KeyError as e:
+		response = _response(400, repr(e), event)
+	return response
 
 
 def _handle(user: str) -> str:
 	with model.RecommendDB(SEED) as db:
 		rec = recommend.Recommender(db, max_clusters=MAX_CLUSTERS, seed=SEED)
 		return rec.recommend(user)
+
+
+def _response(status: int, message: Any, event: Any) -> Dict:
+	return {
+		'statusCode': status,
+		'headers': {'x-custom-header': 'connectfm-recommendation'},
+		'body': {
+			'message': message,
+			'input': event,
+		}
+	}
