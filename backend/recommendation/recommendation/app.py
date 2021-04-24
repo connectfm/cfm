0ba60c4120f1957419dc2310e6b914616579436c
Ryan Tatton <rdt17@protonmail.com>
2021-04-24 11:31:29 -0400

    Adds log level env var
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- backend/recommendation/recommendation/app.py
+++ backend/recommendation/recommendation/app.py
@@ -6,20 +6,18 @@ from typing import Any, Dict
 import model
 import recommend
 
-MAX_CLUSTERS = int(os.environ['MAX_CLUSTERS'])
-if seed := os.environ['SEED'] == 'None':
-	SEED = None
-else:
-	SEED = seed
+MAX_CLUSTERS = int(os.getenv('MAX_CLUSTERS', default=100))
+SEED = os.getenv('SEED')
+LOG_LEVEL = os.getenv('LOG_LEVEL', default=logging.DEBUG)
 
-logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(levelname)s: %(message)s', level=LOG_LEVEL)
 logger = logging.getLogger(__name__)
 
 
 def handle(event, context):
-	logger.debug(f'## ENV VARS\n{jsonpickle.encode(dict(**os.environ))}')
-	logger.debug(f'## EVENT\n{(event := jsonpickle.encode(event))}')
-	logger.debug(f'## CONTEXT\n{jsonpickle.encode(context)}')
+	logger.info(f'## ENV VARS\n{jsonpickle.encode(dict(**os.environ))}')
+	logger.info(f'## EVENT\n{(event := jsonpickle.encode(event))}')
+	logger.info(f'## CONTEXT\n{jsonpickle.encode(context)}')
 	try:
 		recommendation = _handle(event['body'])
 		response = _response(200, recommendation, event)
