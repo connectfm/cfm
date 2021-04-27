173510b8e40de6a0a554b773c43cc725b27e15eb
Alexander Sfakianos <alexander@sfakianos.com>
2021-04-27 09:52:05 -0400

    Adjusted logging to fit AWS docs, fixed a couple of minor bugs

--- backend/dbprocessing/dbprocessing/app.py
+++ backend/dbprocessing/dbprocessing/app.py
@@ -6,22 +6,35 @@ import datetime
 
 import redis_update as redis
 
-logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+# Per AWS documentation - AWS apparently creates its own logging instance with its own metadata
+# Not doing this will lead to some logs not being written. 
+if logging.getLogger().hasHandlers():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+else:
+    logger = logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+    logger = logging.getLogger()
+
 
 def handler(event, context):
     logger.info(f"Handling new DBStream at {datetime.datetime.utcnow()}")
     logger.debug(f"Got event: {event}")
 
     # Update redis values based on changes
-    try:
-        result = redis.update_redis(event)
-        return result
+    # try:
+    result = redis.update_redis(event)
+    return result
 
-    except:
-        e = sys.exc_info()[0]
-        logger.warn(f"Ran into an error when attempting to update Redis: {e}")
-        return {
-            'statusCode': 400,
-            'body': json.dumps("Something went wrong!")
-        }
+    # except:
+    #     e = sys.exc_info()[0]
+    #     logger.warning(f"Ran into an error when attempting to update Redis: {e}")
+    #     return {
+    #         'statusCode': 400,
+    #         'body': json.dumps("Something went wrong!")
+    #     }
+
+
+# For testing -- remove later on.
+if __name__ == '__main__':
+    with open("sample_res.json", 'r') as f:
+        handler(json.load(f), None)
