6dc507d90ab903bf88ddfefb0f1ee3a989ea527c
Ryan Tatton <rdt17@protonmail.com>
2021-04-30 23:46:30 -0400

    Adds comments to suggestions
    Signed-off-by: Ryan Tatton <rdt17@protonmail.com>

--- app/src/main/java/com/cfm/recommend/Recommender.java
+++ app/src/main/java/com/cfm/recommend/Recommender.java
@@ -22,34 +22,50 @@ public class Recommender {
 				Request.Method.GET,
 				endpoint,
 				null,
-				new Response.Listener<JSONObject>() {
-					@Override
-					public void onResponse(JSONObject response) {
-						/*
-						We'd get the call, and probably call the Spotify API for info
-						On the URIs (I've already got a method set up for it in SongService)
+				response -> {
+					/*
+					We'd get the call, and probably call the Spotify API for info
+					On the URIs (I've already got a method set up for it in SongService)
 
-						Then we store the now Song data structures in a List within this object
+					Then we store the now Song data structures in a List within this object
 
-						After all that, we call the callback bc we know the data is compiled and
-						properly stored
-						 */
-						callBack.onSuccess();
+					After all that, we call the callback bc we know the data is compiled and
+					properly stored
+					 */
 
-						/*
-						Then we can do whatever we want down here to give info back to the server
-						abt updated information (We could have prior info as a param)
-						 */
-					}
-				}, new Response.ErrorListener() {
-			@Override
-			public void onErrorResponse(VolleyError error) {
+					/*
+					RESPONSE TO ABOVE (rdt17):
+						I think that is too much responsibility for a single class; too much coupling. Let
+						this class just be responsible for giving you a song URI. Keeping the whole thing as
+						a pipeline of transformations will help keep the logic organized. Using the Java
+						functional API / lambda notation:
+							1. Get recommended song URI: () -> recommender.recommend()
+							2. Get song metadata (album image, artist, etc.): r -> getMetadata(r)
+							3. Add song to the playback queue: s -> queue.add(s)
+						I'm not sure if this actually how it is organized, but entangling the functionality
+						like that will make debugging a nightmare. To go along with the SongService naming
+						convention, the RecommendService, SongService, and PlaybackService should be
+						completely oblivious to each other. They all do (or at least should do) different,
+						independent tasks.
+					 */
+					callBack.onSuccess();
 
-			}
-		}
+					/*
+					Then we can do whatever we want down here to give info back to the server
+					abt updated information (We could have prior info as a param)
+					 */
+
+					/*
+					RESPONSE TO ABOVE (rdt17):
+						There is nothing the server needs to know. All it does is gives recommendations. All
+						data it receives is from ElastiCache, which is after its been saved in DataStore,
+						synced to DynamoDB, and then processed by Alexs' Lambda function to be put into the
+						cache.
+					 */
+				}, error -> {
+
+				}
 		);
-		// Connect to the recommendation system -- may be able to do this through Amplify
-		// Request a recommendation in the form of a song URI
 	}
 
 }
